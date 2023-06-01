0b98b9af3fe5985e673d088245f3020202ea3702
shivakrishnak13 <kosarishivakrishna@gmail.com>
2023-06-01 22:29:05 +0530

    myPortfolio

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import './App.css';
 import About from './Components/About';
 import ContactForm from './Components/Contactme';
@@ -7,19 +8,34 @@ import Projects from './Components/Projects';
 import ScrollToTopButton from './Components/Scroll';
 import Statistics from './Components/Statistics';
 import Techstacks from './Components/Techstacks';
+import { ChakraProvider } from '@chakra-ui/react';
+import Footer from './Components/Footer';
 
 
 function App() {
+
+  
+    const divRef = useRef(null);
+  
+    const handleScroll = () => {
+      const scrollTop = divRef.current.scrollTop;
+      console.log(scrollTop); // Scroll Y value of the div
+    };
+  
+
+
   return (
     <div className="App">
+      
      <Navbar/>
      <Intro/>
-     <About/>
+     <About ref={divRef} func={handleScroll}/>
      <Techstacks/>
      <Projects/>
      <Statistics/>
     <ContactForm/>
     <ScrollToTopButton/>
+    <Footer/>
     </div>
   );
 }
